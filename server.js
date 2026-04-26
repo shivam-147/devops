0ee92257a66d32a00849685fb72f66611f9f341d
shivam <shivam.bathre@reak.in>
2026-04-26 13:50:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('server server running on port 3000')
+})
+
+app.listen(PORT, () => {
+    console.log('Server is healthy and up on port,', PORT)
+})
